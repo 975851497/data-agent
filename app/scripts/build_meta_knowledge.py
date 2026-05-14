cf9c09a8efdbc6724cfdb703fcadbe3681842658
975851497 <7767701+z975851497@user.noreply.gitee.com>
2026-05-14 12:11:35 +0800

    feat(开发)：【重新开发。】脚本中，入库时候，repo对象需要拿到会话session，定义并且传入。切记，seesion使用完需要释放！！

--- app/scripts/build_meta_knowledge.py
+++ app/scripts/build_meta_knowledge.py
@@ -3,24 +3,34 @@ from pathlib import Path
 
 from Lib import argparse
 
+from app.clients.mysql_client_manager import meta_mysql_client_manager
 from app.repositories.msyql.meta_mysql_repository import MetaMysqlRepository
 from app.services.meta_knowledge_service import MetaKnowledgeService
 
 
 async def build(file_path):
-    # 创建业务service对象
-    """
-    入库时，需要repository对象，所以--->先创建
-    """
-    # 创建repository对象
-    meta_mysql_repository = MetaMysqlRepository()
 
-    meta_knowledge_service = MetaKnowledgeService(
-        meta_mysql_repository=meta_mysql_repository,
-    )
-    # 调用业务函数
-    # 又但因为业务执行需要用到当前的配置 --> 把filepath给他。 其次 是异步，所以加await
-    await meta_knowledge_service.build(file_path)
+    # 初始化客户端对象
+    meta_mysql_client_manager.init() # 这之后，就有session factory
+    # 获取session
+    async with meta_mysql_client_manager.session() as meta_session:
+
+        """
+        入库时，需要repository对象，所以--->先创建
+        """
+        # 创建repository对象
+        meta_mysql_repository = MetaMysqlRepository(meta_session)
+
+        # 创建业务service对象
+        meta_knowledge_service = MetaKnowledgeService(
+            meta_mysql_repository=meta_mysql_repository,
+        )
+        # 调用业务函数
+        # 又但因为业务执行需要用到当前的配置 --> 把filepath给他。 其次 是异步，所以加await
+        await meta_knowledge_service.build(file_path)
+
+    # 调用完成，一定要释放资源
+    await meta_mysql_client_manager.close()
 if __name__ == '__main__':
 
 
